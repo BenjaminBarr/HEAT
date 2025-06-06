96735ada51822382247bbf7d954c185b7521bfc2
BenjaminBarr <94140800+BenjaminBarr@users.noreply.github.com>
2025-06-06 15:48:33 -0500

    Add files via upload

--- HEAT/VO2 Calculation.R
+++ HEAT/VO2 Calculation.R
@@ -1,3 +1,5 @@
+install.packages("gghighlight")
+
 library(here) # here makes a project transportable
 library(janitor) # clean_names
 library(readxl) # read excel, duh!
@@ -29,6 +31,8 @@ library(insight)
 library(lazyWeave)
 library(dplyr)
 library(mgcv)
+library(gghighlight)
+
 getwd()
 
 df <- read_excel("./Terrell.VO2.peak.xlsx")
@@ -37,26 +41,36 @@ names(df)
 names(df)<- paste(names(df), df[1,])
 names(df)
 
-df$`VO2 mL/min` <- as.numeric(df$`VO2 mL/min`)
+df$`VO2/kg mL/min/Kg` <- as.numeric(df$`VO2/kg mL/min/Kg`)
 df$`HR bpm` <- as.numeric(df$`HR bpm`)
+df$`Power Watt` <- as.numeric(df$`Power Watt`)
 
 names(df) <- tolower(names(df))
-names(df)[15] <- "vo2"
+names(df)[22] <- "vo2"
 names(df)[24] <- "hr"
+names(df)[37] <- "power"
 
-df1 <- df[c(15,24)]
+df1 <- df[c(22,24,37)]
 df1 <- df1[-c(1:2),]
+df1[1,3] <- 0
+max(df1$power)
 
-is.na(df$vo2)
-is.na(df$hr)
-
+is.na(df1$vo2)
+is.na(df1$hr)
+df1
 df1 <- df1 %>%
-  mutate(smooth_y = predict(gam(vo2 ~ s(hr, bs = "cs"), data = df1)))
+  mutate(smooth_y = predict(gam(hr ~ s(vo2, bs = "cs"), data = df1)))
 
 max(df1$smooth_y)
+max(df1$smooth_y)*.7
+nrow(df1)
 
-ggplot(df1, aes(x = hr, y = vo2)) +
-  geom_point() +
-  geom_smooth() +
+which(df1$smooth_y %in% max(df1$smooth_y)*.7 | max(df1$smooth_y)*.75)
+
+ggplot(df1, aes(x = vo2, y = hr)) +
+  geom_point(data = df1, aes(color = power)) +
+  geom_point(data = df1, aes(x = vo2, y = smooth_y), color = "blue") +
+  scale_color_gradient(low = "yellow", high = "red")+
+  geom_smooth(color = "black") +
   geom_hline(yintercept = max(df1$smooth_y)) +
-  geom_point(data = df1, aes(x = hr, y = smooth_y), color = "red")
+  gghighlight(max(smooth_y) > 115.993)
